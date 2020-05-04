bf36df4848d5049404766865195b706a9591fa65
Andrey Sobol <baskerville@andreysobol.com>
2020-05-04 19:40:08 +0300

    100 trades

--- pseudoorderbook.py
+++ pseudoorderbook.py
@@ -1,3 +1,9 @@
 from xy import exchange
 
-print(exchange({"x":100.0, "y":100.0}, 1, "x"))
+#print(exchange({"x":100.0, "y":100.0}, 1, "x"))
+
+pool = {"x":100.0, "y":100.0}
+for item in range(0, 100):
+    res = exchange(pool, 1, "x")
+    print(res)
+    pool = res[1]
--- xy.py
+++ xy.py
@@ -1,15 +1,15 @@
-pool = {"x":100.0, "y":100.0}
+#pool = {"x":100.0, "y":100.0}
 
 def exchange(pool_before, size, asset):
     pool_after = {}
     if asset == "x":
         pool_after["x"] = pool_before["x"] + size
         pool_after["y"] = (pool_before["y"] * pool_before["x"]) / pool_after["x"]
-        change_result = pool_after["y"] - pool_before["y"]
+        change_result = pool_before["y"] - pool_after["y"]
     else:
         pool_after["y"] = pool_before["y"] + size
         pool_after["x"] = (pool_before["x"] * pool_before["y"]) / pool_after["y"]
-        change_result = pool_after["x"] - pool_before["x"]
+        change_result = pool_before["x"] - pool_after["x"]
     return (change_result, pool_after)
 
-print(exchange(pool, 1, "x"))
+#print(exchange(pool, 1, "x"))
